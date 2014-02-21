122c259095d904f4d4c45b0e71908c06f1da2c0a
niosus <zabugr@gmail.com>
2014-02-21 20:43:27 +0100

    updated actions, something is wrong

--- formulate_problem.m
+++ formulate_problem.m
@@ -9,19 +9,19 @@ function [P, R] = formulate_problem(filename, car_cost)
     # vertical connections
     indicators(parking_rows:parking_rows:end) = 0;
     P{1} = P{1} + diag(indicators, 1);
-    P{1} = P{1} + diag(indicators, -1);
+    P{2} = P{2} + diag(indicators, -1);
     costs = indicators .* -car_cost;
     R{1} = R{1} + diag(costs, 1);
-    R{1} = R{1} + diag(costs, -1);
+    R{2} = R{2} + diag(costs, -1);
     # add horizontal connections
     indicators = zeros(1, columns(R{1}) - parking_rows);
     indicators(1:parking_rows:end) = 1;
     indicators(parking_rows:parking_rows:end) = 1;
-    P{1} = P{1} + diag(indicators, parking_rows);
-    P{1} = P{1} + diag(indicators, -parking_rows);
+    P{3} = P{3} + diag(indicators, parking_rows);
+    P{4} = P{4} + diag(indicators, -parking_rows);
     costs = indicators .* -car_cost;
-    R{1} = R{1} + diag(costs, parking_rows);
-    R{1} = R{1} + diag(costs, -parking_rows);
+    R{3} = R{3} + diag(costs, parking_rows);
+    R{4} = R{4} + diag(costs, -parking_rows);
 
     # define goal
     goal = coords{end}; # for now - the last pos
@@ -31,20 +31,28 @@ function [P, R] = formulate_problem(filename, car_cost)
         dist = get_distance(coords{i}, goal);
         rewards(i) = -dist;
     endfor
-    row_probs = (1 - occupancy_vector') ./ sum(P{1},2);
-    row_probs = repmat(row_probs, 1, columns(P{1}));
-    P{1} = P{1} .* row_probs; # assign probabilities
+    %row_probs = (1 - occupancy_vector') ./ sum(P{1},2);
+    %row_probs = repmat(row_probs, 1, columns(P{1}));
+    %P{5} = P{5} .* row_probs; # assign probabilities
     
-    R{1} = [R{1} rewards']; # append rewards to R{1} as a column
-    P{1} = [P{1} occupancy_vector']; # same for P{1}
-    rsize = size(R{1});
+    R{5} = [R{5} rewards']; # append rewards to R{1} as a column
+    P{5} += diag(1 - occupancy_vector);
+    P{5} = [P{5} occupancy_vector']; # same for P{1}
+    
+    rsize = size(R{5});
     rsize(1) += 1; # increment size
-    R{1} = resize(R{1}, rsize); # make the matrix square again
-    P{1} = resize(P{1}, rsize); # same for P{1} matrix
+    for i = 1:5
+        R{i} = resize(R{i}, rsize);
+        P{i} = resize(P{i}, rsize);
+        difference = 1 - sum(P{i}, 2);
+        P{i} = P{i} + diag(difference);
+        R{i} = sparse(R{i});
+        P{i} = sparse(P{i});
+    endfor
+    P{5}(end,end) = 1;
+    P{4}(end,end) = 1;
+    P{3}(end,end) = 1;
+    P{2}(end,end) = 1;
     P{1}(end,end) = 1;
-    # now make the matrices sparse
-    P{1} = sparse(P{1});
-    R{1} = sparse(R{1});
-    
 endfunction
 
--- init_P_R.m
+++ init_P_R.m
@@ -4,9 +4,11 @@ function [P, R, gap_period, parking_rows, parking_cols] = init_P_R(str)
    parking_cols = numbers(2);
    gap_period = numbers(3);
    num = parking_rows * parking_cols;
-   P = cell(1);
-   R = cell(1);
-   P{1} = zeros(num);
-   R{1} = zeros(num);
+   P = cell(1,5);
+   R = cell(1,5);
+   for i=1:5
+       P{i} = zeros(num);
+       R{i} = zeros(num);
+   endfor
 endfunction
 
